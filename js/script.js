2ab47e3dfc503f24a5b19a0cdf97b3d57f263e0d
Ren Ivey <haleyrivey@gmail.com>
2021-06-28 00:46:02 -0400

    Basic JavaScript added. Called in OpenWeather.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-var APIKey = "35be878d31562e0b31926bafbd67983b";
-var city;
+// Targeting user input.
+var city = document.getElementById("city-input").value;
+var searchCity = document.getElementById("search-city")
+// API key for OpenWeatherMap.
+var APIKey = "35be878d31562e0b31926bafbd67983b"
 
-function getApi() {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(queryURL)
-    console.log(queryURL)
-    .then(function (response) {
+function weatherTime(){
+console.log("Button clicked. Function started.")
+console.log(city)
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
+    .then (function (response) {
         return response.json();
     })
-    .then(function (data) {
-        console.log(data)
-    })
+    .then (function(data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var cityName = document.createElement('h3');
+            var cityTemp = document.createElement('p');
+            var cityWind = document.createElement('p');
+            var cityHumid = document.createElement('p');
+            var cityUV = document.createElement('p');
+
+            cityName.textContent=data[i].name;
+            cityTemp.textContent=data[i].temp;
+            cityWind.textContent=data[i].wind;
+            cityHumid.textContent=data[i].humidity;
+            cityUV.textContent=data[i].name;
+
+            console.log(cityName);
+
+
+            dashboard-mainContainer.append(cityName);
+            dashboard-mainContainer.append(cityTemp);
+        }
+    });
 }
+
+searchCity.addEventListener('click', weatherTime);
+
