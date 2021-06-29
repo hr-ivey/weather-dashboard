79c4f337ab0fb123cd941702c39aa66c08edb14a
Ren Ivey <haleyrivey@gmail.com>
2021-06-28 21:03:11 -0400

    Added dynamically generated content for five day forecast.

--- js/script.js
+++ js/script.js
@@ -5,34 +5,58 @@ var searchCity = document.getElementById("search-city")
 var APIKey = "35be878d31562e0b31926bafbd67983b"
 
 function weatherTime(){
-console.log("Button clicked. Function started.")
+console.log("Primary city weather function started.")
 fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
     .then (function (response) {
         return response.json();
     })
     .then (function(data) {
         console.log(data);
-        for (var i = 0; i < 5; i++) {
             var cityName = document.createElement("h3");
             var cityTemp = document.createElement("p");
             var cityWind = document.createElement("p");
             var cityHumid = document.createElement("p");
             var cityUV = document.createElement("p");
+            var today = new Date().toISOString().slice(0,10)
 
-            cityName.textContent=data.name;
+            cityName.textContent=data.name + " - " + today;
             cityTemp.textContent= ("Temperature: " + data.main.temp);
             cityWind.textContent=("Wind: " + data.wind.speed);
             cityHumid.textContent=("Humidity: " + data.main.humidity);
-            cityUV.textContent=("UV: " + data.uv);
+            cityUV.textContent=("UV Index: " + data.uv);
 
-            dashboardMain.append(cityName);
-            dashboardMain.append(cityTemp);
-            dashboardMain.append(cityWind);
-            dashboardMain.append(cityHumid);
-            dashboardMain.append(cityUV);
+            dashboardMain.append(cityName, cityTemp, cityWind, cityHumid, cityUV);
             return
+    });
+}
+
+function fiveDay(){
+    console.log("Five day forecast function started.")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey)
+    .then (function (response) {
+        return response.json();
+    })
+    .then (function(data) {
+        console.log(data);
+        for (var i = 0; i < 5; i++) {
+            var fiveDayDiv = document.createElement("div");
+            fiveDayDiv.className= "fiveday col";
+            fiveDaySection.append(fiveDayDiv);
+
+            var fiveDate = document.createElement("p");
+            var fiveTemp = document.createElement("p");
+            var fiveWind = document.createElement("p");
+            var fiveHumid = document.createElement("p");
+
+            fiveDate.textContent= data.list[0].dt_txt;
+            fiveTemp.textContent= ("Temperature: " + data.list[0].main.temp);
+            fiveWind.textContent=("Wind: " + data.list[0].dt_txt);
+            fiveHumid.textContent=("Humidity: " + data.list[0].dt_txt);
+
+            fiveDayDiv.append(fiveDate, fiveTemp, fiveWind, fiveHumid);
         }
     });
 }
 
 searchCity.addEventListener('click', weatherTime);
+searchCity.addEventListener('click', fiveDay);
